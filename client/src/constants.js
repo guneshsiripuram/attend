77b7275bec44e6b1442e81bc0cadea355ed284b3
guneshsiripuram <guneshsiripuram48@gmail.com>
2026-03-22 12:47:52 +0530

    Modify SECTIONS and update API_URL
    Updated sections to include 'E' and 'F'. Changed API URL to the production endpoint.

--- client/src/constants.js
+++ client/src/constants.js
@@ -3,8 +3,8 @@
  */
 
 export const BRANCHES = ['CSE', 'ECE', 'EEE', 'MECH', 'CIVIL', 'AI&ML', 'IT'];
-export const SECTIONS = ['A', 'B', 'C', 'D'];
+export const SECTIONS = ['A', 'B', 'C', 'D','E','F'];
 
 export const COLLEGE_DOMAIN = '@raghuenggcollege.in';
 
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+export const API_URL = 'https://attend-api-xfcb.onrender.com/api';
