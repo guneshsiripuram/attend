a7c3538d8aeebd35307ac0f05f38ff6a3172bbad
Gunesh <guneshsiripuram48@gmail.com>
2026-08-06 15:20:09 +0530

    Pin DB session timezone to Asia/Kolkata so CURRENT_DATE comparisons are IST-consistent

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -5,7 +5,8 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  options: '-c timezone=Asia/Kolkata'
 });
 
 pool.on('error', (err) => {
